b40df57026ca90969df34af9a63908e3baf8fd8f
Admin <admin@mac-admin.local>
2018-10-04 19:06:56 +0300

    Initial Commit

---
+++ realmTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  realmTest
+//
+//  Created by Admin on 04/10/2018.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
