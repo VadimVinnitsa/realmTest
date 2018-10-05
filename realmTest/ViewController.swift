675e850389b8faf76b90b5781823df2823f86a73
Admin <admin@mac-admin.local>
2018-10-05 18:06:04 +0300

    1-2

--- realmTest/ViewController.swift
+++ realmTest/ViewController.swift
@@ -1,73 +1,99 @@
-//
-//  ViewController.swift
-//  realmTest
-//
-//  Created by Admin on 04/10/2018.
-//  Copyright © 2018 Admin. All rights reserved.
-//
-
 import UIKit
 import RealmSwift
 
-class Dog : Object {
-    @objc dynamic var name = "gav"
-    @objc dynamic var age = 10
+
+
+final class DOG6: Object {
+    @objc dynamic var name = "dfdf"
+  @objc dynamic var age = 10
+    
    
 }
-
-class Cat : Object {
-    @objc dynamic var name = "gav"
-    @objc dynamic var age = 10
-    
-}
-
 
 
 
 class ViewController: UIViewController {
-
+   let realm = try! Realm()
+   // var notificationToken: NotificationToken? = nil
+    var notificationToken: NotificationToken? = nil
+  //  let results = realm.objects(Person.self).filter("age > 5")
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         print("did load")
-        
-        let sobaka1 = Dog()
-        sobaka1.name = "1"
-        let sobaka2 = Dog()
-        sobaka2.name = "2"
-        let sobaka3 = Dog()
-        sobaka3.name = "3"
-        
-        let cat1 = Cat()
-        cat1.name = "1"
-        
-        let cat2 = Cat()
-        cat2.name = "2"
-        
-        
-        let realm = try! Realm()
+//  let results = realm.objects(DOG6.self)
+       var ob = DOG6()
+  
         try! realm.write {
-            realm.add(sobaka1)
-            realm.add(sobaka2)
-            realm.add(sobaka3)
-            realm.add(cat1)
-            realm.add(cat2)
-       // realm.deleteAll()
+            realm.add(ob)
         }
-      //  print(Realm.Configuration.defaultConfiguration.fileURL!)
         
-        var array = realm.objects(Dog.self)
+        notificationToken = ob.observe({ (change) in
+            switch change {
+            case .change(let properties):
+                for p in properties {
+                   print("changes \(p)")
+                }
+            case .error(let error):
+                print("An error occurred: \(error)")
+            case .deleted:
+                print("The object was deleted.")
+            }
+        })
         
-        print(array.count)
+//        notificationToken = results.observe { [weak self] (changes: RealmCollectionChange) in
+//
+//            switch changes {
+//
+//            case .initial:
+//                // Results are now populated and can be accessed without blocking the UI
+//                print("initial")
+//            case .update(_, let deletions, let insertions, let modifications):
+//                print("deletions \(deletions)")
+//                print("insertions \(insertions)")
+//                print("modifications \(modifications)")
+//                // Query results have changed, so apply them to the UITableView
+//
+//            case .error(let error):
+//                // An error occurred while opening the Realm file on the background worker thread
+//                fatalError("\(error)")
+//            }
+//        }
+        
+        
+        let dog1 = DOG6(value: ["name":"sirco","age":20])
+        let dog2 = DOG6(value: ["asdf",21])
+
+        try! realm.write {
+//            for dog in realm.objects(DOG.self).filter("age == 10") {
+//                person.age += 1
+//            }
+//            realm.add(dog1)
+//            realm.add(dog2)
+            
+            
+            for dog in realm.objects(DOG6.self) {
+                dog.age += 1
+            }
+            
+            let result = realm.objects(DOG6.self).last
+            realm.delete(result!)
+         //   realm.delete(dog1)
+            
+            
+        }
+        
       
         
-        try! realm.write {
-            realm.delete(sobaka2)
-        }
-       
-        print(array.count)
+  
+      //  print(Realm.Configuration.defaultConfiguration.fileURL)
+   
         
         
+   
         }
+    
+ 
 
 
 }
